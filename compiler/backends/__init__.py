8922105b4bb4d6ffc2f28685adc2dd3600416c1e
erikjjpost <erikjjpost@users.noreply.github.com>
2026-07-26 17:14:38 +0200

    add backend plugin package

---
+++ compiler/backends/__init__.py
@@ -0,0 +1,5 @@
+"""Ingebouwde Beckeringh Palace-backendplugins.
+
+Iedere publieke module in dit package exporteert exact één ``backend``-object.
+De discoverylaag laadt deze modules deterministisch op modulenaam.
+"""
